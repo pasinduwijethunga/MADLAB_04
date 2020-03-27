8da43a6316954b7d00668a25019554e8ae031eca
pasinduwijethunga <pasinduwijethunga98@gmail.com>
2020-03-27 23:20:33 +0530

    Notification

---
+++ app/src/main/java/com/example/notification/Register.java
@@ -0,0 +1,16 @@
+package com.example.notification;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidnotification.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
